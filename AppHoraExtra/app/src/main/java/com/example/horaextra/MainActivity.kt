5fd370934a873b9bd4e3bf4ee9613a03f09308da
Lee Eros Galaad Dias De Moura <95001466+LeeEros@users.noreply.github.com>
2024-09-13 14:22:59 -0300

    Feat: binding

--- AppHoraExtra/app/src/main/java/com/example/horaextra/MainActivity.kt
+++ AppHoraExtra/app/src/main/java/com/example/horaextra/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.example.horaextra
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.horaextra.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.botaoProximo.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        TODO("Not yet implemented")
     }
 }
